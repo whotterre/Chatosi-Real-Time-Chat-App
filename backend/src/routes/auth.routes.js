b1fc23836bf6ad88ed3f74ea3240961555f64590
YourGitHubUsername <YourGitHubEmail>
2025-09-03 22:04:36 +0100

    data base setup

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { login, register, logout } from '../controllers/auth.controller';
+import { login, register, logout } from '../controllers/auth.controller.js';
 const router = express.Router();
 
 router.post('/login', login)
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,17 @@
 import express from "express";
+import dotenv from "dotenv"
+
 import authRoutes from "./routes/auth.routes.js";
+import { connectDB } from "./lib/db.js";
+
+dotenv.config()
 const app = express();
 
+const PORT = process.env.PORT || 3000;
+
 app.use("/api/auth", authRoutes);
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+
+app.listen(PORT, () => {
+  console.log("Server is running on PORT:" + PORT);
+  connectDB();
 });
