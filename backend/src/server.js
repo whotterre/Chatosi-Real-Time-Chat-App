9b11c23c04cf5f115deffcb7b2b9884c873abdb7
YourGitHubUsername <YourGitHubEmail>
2025-09-04 14:20:37 +0100

    completed authenticATION

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,17 @@
 import express from "express";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser"
 
 import authRoutes from "./routes/auth.routes.js";
 import { connectDB } from "./lib/db.js";
 
-dotenv.config()
+dotenv.config();
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
+app.use(cookieParser())
 app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
