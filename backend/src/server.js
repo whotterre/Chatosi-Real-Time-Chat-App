ca0df00b47a6053d2522d9c98682e2c533f9dc4f
bragosi <oladejoboluwatife20003@gmail.com>
2025-09-08 16:52:50 +0100

    fixed routing

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser"
+import cors from "cors"
 
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
@@ -13,6 +14,11 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cookieParser())
+app.use(cors({
+  origin : "http://localhost:5173",
+  credentials :  true 
+}))
+
 app.use("/api/auth", authRoutes)
 app.use("/api/message", messageRoutes);
 
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,32 @@
 import Navbar from "./components/Navbar";
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import RegisterPage from "./pages/RegisterPage";
 import LoginPage from "./pages/LoginPage";
 import SettingsPage from "./pages/SettingsPage";
 import ProfilePage from "./pages/ProfilePage";
+import { useAuthStore } from "./store/useAuthStore";
+import { useEffect } from "react";
+import { Loader } from "lucide-react"
+
 export default function App() {
+  const {authUser, checkAuth, isCheckingAuth} = useAuthStore()
+
+  useEffect(()=>{
+    checkAuth()
+  },[checkAuth])
+  console.log({authUser})
+
+  if (isCheckingAuth && !authUser) return(
+    <div className="flex items-center justify-center h-screen">
+      <Loader className="size-10 animate-spin"/>
+    </div>
+  )
   return (
     <div>
       <Navbar/>
       <Routes>
-        <Route path="/" element={<HomePage/>} />
+        <Route path="/" element={  authUser ? <HomePage/> :  <Navigate to={"/login"} />} />
         <Route path="/register" element={<RegisterPage/>} />
         <Route path="/login" element={<LoginPage/>} />
         <Route path="/settings" element={<SettingsPage/>} />
