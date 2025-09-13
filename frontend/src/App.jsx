6b71d41c7aec05a52a4215fa7c1f2c6b2fb2222a
bragosi <oladejoboluwatife20003@gmail.com>
2025-09-13 09:30:10 +0100

    made the themes consistent across the app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,9 +7,8 @@ import SettingsPage from "./pages/SettingsPage";
 import ProfilePage from "./pages/ProfilePage";
 import { useAuthStore } from "./store/useAuthStore";
 import { useEffect } from "react";
-import { Loader } from "lucide-react";
+import { Loader, MessagesSquare } from "lucide-react";
 import { Toaster } from "react-hot-toast";
-import { MessagesSquare } from "lucide-react";
 import { useThemeStore } from "./store/useThemeStore";
 
 export default function App() {
@@ -20,29 +19,30 @@ export default function App() {
     checkAuth();
   }, [checkAuth]);
 
-  if (isCheckingAuth && !authUser)
-    return (
-      <div className="flex flex-col items-center justify-center h-screen">
-        <Loader className="size-10 animate-spin" />
-        <div className="flex gap-2">
-          <div className="w-9 h-9 rounded-lg bg-primary/10 flex items-center justify-center">
-            <MessagesSquare className="size-10 text-primary" />
-          </div>
-          <h1 className="text-xl font-serif font-bold">Chatosi</h1>
-        </div>
-      </div>
-    );
-
   return (
-    <div data-theme={theme}>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={authUser ? <HomePage /> : <Navigate to="/login" />} />
-        <Route path="/register" element={!authUser ? <RegisterPage /> : <Navigate to="/" />} />
-        <Route path="/login" element={!authUser ? <LoginPage /> : <Navigate to="/" />} />
-        <Route path="/settings" element={<SettingsPage />} />
-        <Route path="/profile" element={authUser ? <ProfilePage /> : <Navigate to="/login" />} />
-      </Routes>
+    <div data-theme={theme} className="min-h-screen">
+      {isCheckingAuth && !authUser ? (
+        <div className="flex flex-col items-center justify-center h-screen">
+          <Loader className="size-10 animate-spin" />
+          <div className="flex gap-2 mt-2">
+            <div className="w-9 h-9 rounded-lg bg-primary/10 flex items-center justify-center">
+              <MessagesSquare className="size-10 text-primary" />
+            </div>
+            <h1 className="text-xl font-serif font-bold">Chatosi</h1>
+          </div>
+        </div>
+      ) : (
+        <>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={authUser ? <HomePage /> : <Navigate to="/login" />} />
+            <Route path="/register" element={!authUser ? <RegisterPage /> : <Navigate to="/" />} />
+            <Route path="/login" element={!authUser ? <LoginPage /> : <Navigate to="/" />} />
+            <Route path="/settings" element={<SettingsPage />} />
+            <Route path="/profile" element={authUser ? <ProfilePage /> : <Navigate to="/login" />} />
+          </Routes>
+        </>
+      )}
       <Toaster />
     </div>
   );
